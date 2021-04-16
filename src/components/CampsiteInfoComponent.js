5308449e08a9303cf7d8ea766d10a142a1215793
Peter Drews <petermdrews@gmail.com>
2021-04-16 16:02:21 -0500

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,65 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class CampsiteInfo extends Component {
+function RenderCampsite({ campsite }) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-
-  renderCampsite(campsite) {
+function RenderComments({ comments }) {
+  if (comments) {
     return (
-      <div className="col-md-5 m1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+      <div className="col-md-5 m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return (
+            <p>
+              <div>{comment.text}</div>
+              <div>
+                <div>
+                  {" "}
+                  -- {comment.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
+                  }).format(new Date(Date.parse(comment.date)))}
+                </div>
+              </div>
+            </p>
+          );
+        })}
       </div>
     );
   }
+  return <div />;
+}
 
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {this.props.campsite.comments.map((comment) => {
-            return (
-              <p>
-                <div>{comment.text}</div>
-                <div>
-                  <div>
-                    {" "}
-                    -- {comment.author},{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    }).format(new Date(Date.parse(comment.date)))}
-                  </div>
-                </div>
-              </p>
-            );
-          })}
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />
         </div>
-      );
-    }
-    return <div />;
+      </div>
+    );
   }
-
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div>
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}{" "}
-            {this.renderComments(this.props.campsite)}
-          </div>
-        </div>
-      );
-    }
-    return <div />;
-  }
+  return <div />;
 }
 
 export default CampsiteInfo;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Directory from "./DirectoryComponent";
+import { CAMPSITES } from "../shared/campsites";
+import Header from "./HeaderComponent.js";
+import Footer from "./FooterComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null,
+    };
+  }
+
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Directory
+          campsites={this.state.campsites}
+          onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
